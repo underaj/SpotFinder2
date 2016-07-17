fe35e7c4e6c1934d46e1df05d80d10ae8bf9107b
Ching <chingyang91@gmail.com>
2016-07-17 16:12:27 -0700

    (feat) setup webpack and render app.jsx

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,3 +1,9 @@
-var server = require('../server.js');
+var morgan = require('morgan');
+var parser = require('body-parser');
 
-module.exports = function () {};
+module.exports = function(app, express) {
+  app.use(morgan('dev'));
+  app.use(parser.json());
+  app.use(express.static(__dirname + '/../../client'));
+  app.use(express.static(__dirname + '/../../compiled'));
+};
